cb015b5999862bdf1b080c8d615cceb5d675d8ba
Felipe Augusto Gonçalves Basilio <43045359+FeehGb@users.noreply.github.com>
2018-09-25 22:47:26 -0300

    Initial commit

---
+++ routes/users.js
@@ -0,0 +1,44 @@
+    let NeDB = require('nedb');
+    let db = new NeDB({
+        filename:'users.db',
+        autoload: true
+    })
+    module.exports = (app)=>{
+        
+        app.get('/users',(req, res)=>{
+            db.find({}).sort({name:-1}).exec((err, users)=>{
+
+                if(err){
+                    console.log(`erro: ${$err}`);
+                    res.status(400).json({
+                        error:err
+                    })
+                } else {
+                    
+                    res.statusCode = 200;
+                    res.setHeader('content-type','aplication/json');
+                    res.json({
+                        users
+                    });
+                }
+
+            });
+        });
+
+    
+        app.post('/users',(req, res)=>{
+           
+            db.insert(req.body,(err, user) =>{
+                if(err){
+                    console.log(`Error: ${err}`);
+                    res.status(400).json({
+                        "error":err
+                    });
+                }else{
+                    res.status(200).json(user);
+                }
+            });
+    
+        });
+
+    };
